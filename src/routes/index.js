b66724f65148ba370a765fce1d250dcbaa626e20
AngelesMorales513 <56412318+AngelesMorales513@users.noreply.github.com>
2020-04-03 17:36:31 -0600

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
- res.render('index');
+ res.redirect('index');
 });
 
 router.get('/about', (req, res) => {
